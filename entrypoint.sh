a0e31fc0ed44609b632518c30a25801bcf0ae881
aladdin4220243 <167805619+aladdin4220243@users.noreply.github.com>
2026-04-26 02:31:46 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,6 @@ cd /cowrie
 
 echo "==> Starting Cowrie..."
 
-mkdir -p etc var/log/cowrie var/lib/cowrie/downloads
-
-if [ ! -f etc/ssh_host_rsa_key ]; then
-    ssh-keygen -t rsa -b 2048 -f etc/ssh_host_rsa_key -N "" -q -m PEM
-    echo "==> RSA key generated"
-fi
-
-chmod 600 etc/ssh_host_rsa_key
-
 echo "==> Starting forwarder..."
 python /cowrie/forwarder.py &
 
